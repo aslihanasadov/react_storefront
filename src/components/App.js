08c9684f2f2b0cf0f55b266c61c789cac3c1fa69
xshigeox <59002383+xshigeox@users.noreply.github.com>
2020-05-19 15:47:56 -0400

    navbar link updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,34 @@
 import React from "react"
-import { Route, Switch, BrowserRouter, Link } from "react-router-dom";
-import IndexCategories from "../containers/IndexCategories";
+import { Route, Switch, BrowserRouter, Link, Redirect } from "react-router-dom"
+import IndexCategories from "../containers/IndexCategories"
 import Footer from "./Footer"
 import NavBar from "./NavBar"
 
 const App = (props) => {
   return (
     <BrowserRouter>
-      <div className="">
-        <h1 className="title">
-          React Storefront
-        </h1>
-        <nav>
-          <NavBar />
-        </nav>
-        <div className="paths">
-          <Switch>
-            <Route exact path="/store" component={IndexCategories} />
-          </Switch>
-        </div>
+      <nav>
+        <NavBar />
+      </nav>
+
+      <div className="paths">
+        <Switch>
+          <Redirect exact path="/" to="/store" />
+          <Route exact path="/store" component={IndexCategories} />
+          <Route exact path="/store/tops" />
+          <Route exact path="/store/bottoms" />
+          <Route exact path="/store/sportsBras" />
+          <Route exact path="/store/shoes" />
+          <Route exact path="/new" />
+          <Route exact path="/cart" />
+        </Switch>
+      </div>
+
+      <div className="footer">
         <Footer />
       </div>
     </BrowserRouter>
-  );
+  )
 }
 
 export default App
--- src/components/CategoriesTile.js
+++ src/components/CategoriesTile.js
@@ -1,20 +1,18 @@
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+import React, { Fragment } from "react"
+import { Link } from "react-router-dom"
 import _ from "lodash"
 
-const CategoriesTile = props => {
+const CategoriesTile = (props) => {
   const name = _.startCase(props.name)
-  
-  return(
+
+  return (
     <div>
-      <div>
-        <h3>{name}</h3>
-        <Link to={`store/${props.name}`}>
-          <p>Category Image??</p>
-        </Link>
-      </div>
+      <h3>{name}</h3>
+      <Link to={`store/${props.name}`}>
+        <p>Category Image??</p>
+      </Link>
     </div>
   )
 }
 
-export default CategoriesTile;
+export default CategoriesTile
