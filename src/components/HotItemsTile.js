b5690620cea451bdf3976b821ec6aaa5d301d288
Andisa Montez <andisa.andisa@gmail.com>
2020-05-20 03:44:15 -0400

    navbar padding, hot items added

---
+++ src/components/HotItemsTile.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from "react";
+
+
+const HotItems = props => {
+  const [hotItems, setHotItems] = useState([]);
+
+  return (
+      <div>
+        <h1>Hot Items!</h1>
+      {/*  list of name, category, picture*/}
+      </div>
+  )
+}
+
+export default HotItems
