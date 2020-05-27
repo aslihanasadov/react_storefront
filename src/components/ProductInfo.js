ad210b93f61391cc971ae2057c96bc3591be47aa
xshigeox <59002383+xshigeox@users.noreply.github.com>
2020-05-26 22:03:15 -0400

    moved search to all pages

--- src/components/ProductInfo.js
+++ src/components/ProductInfo.js
@@ -1,7 +1,5 @@
-import React, { useState, Fragment } from "react"
+import React, { useState, useEffect } from "react"
 import Snackbar from "@material-ui/core/Snackbar"
-import IconButton from "@material-ui/core/IconButton"
-import CloseIcon from "@material-ui/icons/Close"
 import MuiAlert from "@material-ui/lab/Alert"
 import { makeStyles } from "@material-ui/core/styles"
 
